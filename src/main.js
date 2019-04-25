2b91b10227c0779c6de4f2048197794af19a89c2
luisaromero <luisa.marcela.romero@gmail.com>
2019-04-25 09:47:16 -0400

    Subiendo cambios en filtrado por nombre y agregando imagen de fondo

--- src/main.js
+++ src/main.js
@@ -1,1 +1,9 @@
 /* Manejo del DOM */
+let todos = POKEMON.pokemon;
+let name = "Pikachu";
+
+let filtrarNombre = (name) => {
+   return todos.filter(element => element.name===name)
+}
+
+console.log(filtrarNombre(name));
