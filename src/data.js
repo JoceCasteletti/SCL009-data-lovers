c2dd2d641fe1724b6366a650046e95eae0f60595
luisaromero <luisa.marcela.romero@gmail.com>
2019-05-09 09:49:27 -0400

    terminando readme

--- src/data.js
+++ src/data.js
@@ -73,13 +73,13 @@ window.orderPokemon = orderPokemon
 //   return result;
 // }
 // window.computeStats = computeStats;
-const counterEggs = (data,condition) => {
-let counterData = data.reduce(function(counter,data){
-  if (data.egg.includes(condition)) {
-  return counter +1 * 100 /151 ; 
- } else {
-  return counter ;
- }
-   },0)}
+// const counterEggs = (data,condition) => {
+// let counterData = data.reduce(function(counter,data){
+//   if (data.egg.includes(condition)) {
+//   return counter +1 * 100 /151 ; 
+//  } else {
+//   return counter ;
+//  }
+//    },0)}
    
-window.counterEggs = counterEggs
+// window.counterEggs = counterEggs
